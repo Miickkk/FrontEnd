a2daa2d8657fbc458c1d12d28952511da6480155
Miickkk <anickmurilomeyetuti@gmail.com>
2025-02-28 11:29:28 -0300

    28.02.25

---
+++ DOM/Exercicio1DOM/script.js
@@ -0,0 +1,10 @@
+function botaoDOM() {
+  document.getElementById("titulo1").innerText = "Outro Titulo";
+}
+
+let titulo = document.getElementById("titulo1");
+console.log(titulo1);
+
+let descricao = document.getElementsByClassName("parágrafo"); //vetor -> array
+console.log(paragrafo);
+
