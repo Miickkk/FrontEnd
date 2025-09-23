27fd50129c0f4bd63804ac607d6e154231eb674c
Miickkk <anickmurilomeyretuti@gmail.com>
2025-09-22 22:14:18 -0300

    22.09.25

--- Frameworks/Angular20/SA_IMOPRI_AUTH/src/app/templates/components/footer/footer.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-footer',
-  standalone: true,
-  templateUrl: './footer.html',
-  styleUrls: ['./footer.scss']
-})
-export class Footer {}  // ⚠️ Export precisa bater com o import no App
--- Frameworks/Angular20/SA_IMOPRI_AUTH/src/app/templates/components/header/header.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header',
-  standalone: true,
-  templateUrl: './header.html',
-  styleUrls: ['./header.scss']
-})
-export class Header {}  // ⚠️ Export precisa bater com o import no App
